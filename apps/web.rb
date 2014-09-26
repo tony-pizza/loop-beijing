5dd8c829167d4a6eecd4c7e38c4a310dedecf6d7
Ian Pearce <ian@ianpearce.org>
2014-09-26 17:04:34 +0800

    Web updates

--- apps/web.rb
+++ apps/web.rb
@@ -5,73 +5,63 @@ class Web < Sinatra::Application
   set :public_dir, settings.root + '/public'
   set :slim, pretty: settings.development?
 
-  get /^\/\w*(\.html?)?$/ do
-    <<-EOHTML
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Interactive and participatory audio installations on Beijing public buses." />
-  <meta name="keywords" content="loop, beijing, loop beijing, buses, beijing design week, audio installation" />
-	<title>LOOP Beijing</title>
-  <style type="text/css">
-    body {
-      background: url(/images/bg.jpg) no-repeat fixed;
-      background-size: cover;
-      color: #FFFFFF;
-      font-family: Helvetica, Arial, "Microsoft Yahei", "微软雅黑", 黑体, SimHei, "华文黑体", STHeiti, STXihei, "华文细黑", sans-serif;
-      font-size: 100%;
-      line-height: 1.5em;
-      text-align: center;
-    }
-  </style>
-</head>
-<body>
-<div style="top: 20%; position: absolute; width: 100%;">
-  <img src="/images/logo.png" />
-</div>
-</body>
-</html>
-    EOHTML
+  paths root:           '/',
+        bus:            '/bus/:line',
+        recordings:     '/recordings',
+        new_recording:  '/recordings/new'
+
+  get /^\/(index(\.html?))?$/ do
+    @body_id = 'landing'
+    @buses = Recording.reorder(nil).uniq.pluck(:bus)
+    slim :index
   end
 
-  not_found do
-    redirect '/'
+  get :bus do
+    if params[:line] != params[:line].to_i.to_s
+      redirect path_to(:bus).with(params[:line].to_i)
+    end
+    @recordings = Recording.where(bus: params[:line])
+    slim :bus
   end
 
-  error do
-    redirect '/'
-  end
-
-  get '/new' do
-    redirect '/' unless settings.development?
-    transloadit_params = JSON.generate(
+  get :new_recording do
+    @transloadit_params = JSON.generate(
       auth: {
         key:     ENV['TRANSLOADIT_AUTH_KEY'],
         expires: (Time.now + 10.minutes).utc.strftime('%Y/%m/%d %H:%M:%S+00:00')
       },
       template_id: ENV['TRANSLOADIT_TEMPLATE_ID']
     )
-    digest    = OpenSSL::Digest.new('sha1')
-    signature = OpenSSL::HMAC.hexdigest(digest, ENV['TRANSLOADIT_SECRET_KEY'], transloadit_params)
-    slim :new, locals: { digest: digest, signature: signature, transloadit_params: transloadit_params }
+
+    @signature = OpenSSL::HMAC.hexdigest(
+      OpenSSL::Digest.new('sha1'),
+      ENV['TRANSLOADIT_SECRET_KEY'],
+      @transloadit_params)
+
+    slim :new
   end
 
-  post '/upload' do
-    redirect '/' unless settings.development?
-
+  post :recordings do
     transloadit_results = JSON.parse(params['transloadit'])['results']
     original = transloadit_results[':original'].first
     mp3 = transloadit_results['mp3'].first
 
-    Recording.create!(
+    recording = Recording.create!(
       bus: params[:recording][:bus],
       original_url: original['url'],
       web_url: mp3['url'],
       duration: original['meta']['duration']
     )
 
+    redirect path_to(:bus).with(recording.bus)
+  end
+
+
+  not_found do
+    redirect '/'
+  end
+
+  error do
     redirect '/'
   end
 
