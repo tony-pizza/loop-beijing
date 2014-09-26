bbd24baaa0649b80dab556211e7a629613a24c5e
Ian Pearce <ian@ianpearce.org>
2014-09-27 02:05:46 +0800

    Make it easier to update menu url

--- apps/phone.rb
+++ apps/phone.rb
@@ -26,7 +26,7 @@ class Phone < Sinatra::Application
 
   helpers do
     def voice_url(name)
-      "#{ENV['S3_BASE_URL']}/phone-menu/#{name}.wav"
+      "#{ENV['MENU_RECORDINGS_BASE_URL']}/#{name}.wav"
     end
   end
 
